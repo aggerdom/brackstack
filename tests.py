9d4ff19baf38241453efe980a8e2128b1afe0ce0
Alex Gerdom <alexgerdom@gmail.com>
2016-05-13 23:01:07 -0400

    Added documentation, reordered methods to emphasise public methods, and moved brackstack4 to brackstack

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from brackstack4 import *
+from brackstack import *
 from gui_interface import tk_display_text
 
 def test_nesting_depths(userstring):
@@ -69,3 +69,6 @@ def test():
     test_constructor_recognizes_default_pairs("('hello)')",[0,1,1,1,1,1,1,1,1,0])
     test_constructor_recognizes_default_pairs("(((foo)]))",[0,1,2,3,3,3,2,2,1,0])
     test_constructor_recognizes_default_pairs("'''foo'"   ,[0,0,0,0,0,0,0])
+
+if __name__ == '__main__':
+    test()
